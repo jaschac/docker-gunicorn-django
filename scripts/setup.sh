fafa17160091bdcf9c4a7c8b8591fa97553d4256
Jascha Casadio <jaschacasadio@gmail.com>
2015-11-11 23:28:26 +0100

    The client can now define the number of workers of GUnicorn. It defaults to 2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,12 +4,15 @@ set -e
 
 WWW_ROOT_DIR=/var/www/webapp
 
+# Initialize the workers
+ [[ -z $WEB_APP_WORKERS ]] && WORKERS=2 || WORKERS=$WEB_APP_WORKERS
+
 if [ -d $WWW_ROOT_DIR ]
 then
 	cd $WWW_ROOT_DIR
 	WEB_APP=$(find * -maxdepth 0 -type d)
 	if [ -d $WEB_APP ]
 	then
-		gunicorn $WEB_APP.wsgi:application -w 2 --bind=0.0.0.0:8001
+		gunicorn $WEB_APP.wsgi:application -w $WORKERS --bind=0.0.0.0:8001
 	fi
 fi
