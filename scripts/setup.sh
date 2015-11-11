f055832e81cbd0fd49e438dc50cb21483bcd6aa2
Jascha Casadio <jaschacasadio@gmail.com>
2015-11-11 23:29:46 +0100

    Added error handling in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Initializes a gunicorn-django container
+#
 
 set -e
 
@@ -10,12 +13,12 @@ WWW_ROOT_DIR=/var/www/webapp
 # Initialize the port
 [[ -z $WEB_APP_PORT ]] && PORT=8001 || PORT=$WEB_APP_PORT
 
-if [ -d $WWW_ROOT_DIR ]
+# Make sure the webapp directory exists
+if [ -d $WWW_ROOT_DIR ] && [ "$(ls -A $WWW_ROOT_DIR)" ]
 then
 	cd $WWW_ROOT_DIR
 	WEB_APP=$(find * -maxdepth 0 -type d)
-	if [ -d $WEB_APP ]
-	then
-		gunicorn $WEB_APP.wsgi:application -w $WORKERS --bind=0.0.0.0:$PORT
-	fi
+	gunicorn $WEB_APP.wsgi:application -w $WORKERS --bind=0.0.0.0:$PORT
+else
+	exit 1
 fi
